8c40707de61a221516018ef9b557b1e043f16df1
cristianomatos <cristianobmatos@gmail.com>
2014-12-29 16:04:53 -0200

    AOKP animations: Add an entry for TRANSIT_TASK_OPEN_BEHIND [1/2]
    Change-Id: I306c932c79d32e727f15fe1645ffa723d3169bec
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3721,7 +3721,6 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
-<<<<<<< HEAD
          * @hide
          */
         public static final String SHOW_SU_INDICATOR = "show_su_indicator";
@@ -3811,7 +3810,7 @@ public final class Settings {
         /**
          * AOKP Custom System Animations
          * @hide
-         */
+         */  
         public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
                 "activity_open",
                 "activity_close",
@@ -3823,7 +3822,8 @@ public final class Settings {
                 "wallpaper_close",
                 "wallpaper_intra_open",
                 "wallpaper_intra_close",
-        };
+                "task_open_behind",
+        };  
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
         public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
