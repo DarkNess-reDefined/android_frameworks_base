587a7eefda6b89ed1eb009d7128c5a34fafba9f9
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-12-24 17:06:09 +0100

    Statusbar Network Indicators [1/2]
    Change-Id: Iebde8661a00ef75390fc652245dd3eeb91a46ece
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3721,6 +3721,13 @@ public final class Settings {
         */
         public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
 
+        /**
+         * Whether to disable showing arrows in network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
