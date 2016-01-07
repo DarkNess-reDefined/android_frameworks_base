6c14b02f4206dff1d5dc5c96733b26520d20234d
Christian Oder <myself5@carbonrom.org>
2016-01-07 19:59:10 +0100

    [1/3] fwb: Add ability to permanently hide apps from recents
    This is based off XposedAppSettings, but modified to fit our needs, and
    a little bit of: d9973ba
    Part 2 is inside the Lux CarbonFibers Bringup
    
    Change-Id: Ie77f6954d3150a1c647c9bd29f055ad0ddb0f1df
    
    P.S. 2: fwb: fix HIDE_FROM_RECENTS_LIST provider entry
    
    Change-Id: I859f2c9561fc7fc77c57d8dad5f1ba6a8033e21b
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3701,6 +3701,7 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+<<<<<<< HEAD
          * Whether to prevent loud volume levels when headset is first plugged in.
          * @hide
          */
@@ -3770,6 +3771,23 @@ public final class Settings {
          * @hide
          */
         public static final String BRIGHTNESS_ICON = "brightness_icon";
+         * AOKP Custom System Animations
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
          * Toast animations
@@ -3816,7 +3834,7 @@ public final class Settings {
         /**
          * AOKP Custom System Animations
          * @hide
-         */  
+         */
         public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
                 "activity_open",
                 "activity_close",
@@ -3829,7 +3847,7 @@ public final class Settings {
                 "wallpaper_intra_open",
                 "wallpaper_intra_close",
                 "task_open_behind",
-        };  
+        };
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
         public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
@@ -3838,6 +3856,12 @@ public final class Settings {
          * @hide
          */
         public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
+
+        /**
+        * List of Apps hidden from recents
+        * @hide
+        */
+        public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
 
 
         /**
