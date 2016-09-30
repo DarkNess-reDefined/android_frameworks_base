9791ad318ad44997248d9969e944264a3ae5f0db
Cristiano Matos <cristianobmatos@gmail.com>
2016-09-30 20:44:51 +0200

    Fw_base - Ability to hide superuser status bar icon [1/2]
    - Add a simple option to hide the su icon placed in
    status bar when a session is active
    
    Forward ported from crDroid sources:
    https://github.com/crdroidandroid/android_frameworks_base/commit/d8a9b6dedc2bc009a152d942a71b2dfb62a24b53
    
    AICPfy:
    - Bring it back to N by @LorDClockaN
    
    Change-Id: I68c90ac0fb4a83f613ea0d57d0319dddff3f3990
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3907,6 +3907,11 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
