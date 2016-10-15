698733b2ca1427efca69d7af758498791e818dc1
bigrushdog <randall.rushing@gmail.com>
2016-10-15 15:05:24 +0200

    DUI: Initial DUI checkin for N
    Change-Id: I9a30dc91b01a84e5c348e07ad003acc49b86155b

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10460,6 +10460,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public boolean hasNavigationBar() {
         return mPolicy.hasNavigationBar();
     }
