bce750824312c42590ceb57c2435ab7456b13236
yuanhuihui <yuanhuihui@xiaomi.com>
2016-10-20 21:43:44 +0800

    Clean up pending broadcast receivers when force stopping package
    When an app is force stopped, there may still exist broadcast receviers in pending
    list. Make sure we clean up it up completely.
    
    Change-Id: Ic62241b04863efb51448924c9387cee844e49739

--- services/core/java/com/android/server/am/BroadcastQueue.java
+++ services/core/java/com/android/server/am/BroadcastQueue.java
@@ -1457,6 +1457,14 @@ public final class BroadcastQueue {
             }
         }
 
+        if(mPendingBroadcast != null){
+            didSomething |= mPendingBroadcast.cleanupDisabledPackageReceiversLocked(
+                    packageName, filterByClasses, userId, doit);
+            if (!doit && didSomething) {
+                return true;
+            }
+        }
+
         return didSomething;
     }
 
