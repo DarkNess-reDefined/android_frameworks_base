ea8a0e84fabe57f3beff6949035ef6ade5885c23
Nathan Chancellor <natechancellor@gmail.com>
2016-11-12 01:41:59 -0700

    Fully remove ContentObserver from Bluetooth battery level toggle
    I forgot to leave this out in the switch to a SettingsObserver...
    
    The registering of this Observer was removed so it is doing nothing now.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Change-Id: I0f1c73daa4246ada9ba0fb5a2dd5e7a6f06e8db3
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -265,15 +265,6 @@ public class PhoneStatusBarPolicy implements Callback, RotationLockController.Ro
         mDataSaver.addListener(this);
     }
 
-    private ContentObserver mBTIconObserver = new ContentObserver(null) {
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            mShowBluetoothBattery = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.BLUETOOTH_SHOW_BATTERY, 0) == 1;
-            updateBluetooth();
-
- 
-
     private ContentObserver mSettingsObserver = new ContentObserver(null) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
