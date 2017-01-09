8f448a898a4e60c36a2c107a15b92fd468d65b5d
GreenArrow9 <catbat.pc@gmail.com>
2017-01-09 04:03:42 +0000

    Remove dashboard tile summaries [2/2] Change-Id: I028523a50949741d11e61d88cd50891404a52b26

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3701,7 +3701,6 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
-<<<<<<< HEAD
          * Whether to prevent loud volume levels when headset is first plugged in.
          * @hide
          */
@@ -3824,6 +3823,12 @@ public final class Settings {
          * @hide
          */
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+        /**
+         * Remove tile summaries in settings
+         * @hide
+         */
+        public static final String REMOVE_TILE_SUMMARY = "remove_tile_summary";
 
         /**
          * Disabled Transition animations of the android system
