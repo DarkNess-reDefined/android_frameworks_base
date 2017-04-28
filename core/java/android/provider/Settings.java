5caf58a83f4e50e1957e90d33ee345f6d9f8839e
AKabhishek <abhishekkaushik.star@gmail.com>
2017-04-28 16:59:20 +0000

    Fwb: Updates
    - Fix up mismerge
    - Move cm_colors > dnd_colors & dimens dnd_dimens
    - Make Network traffic meter smaller thanks to @kecinzer

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3722,6 +3722,28 @@ public final class Settings {
         public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
 
         /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Whether or not to hide the network traffic indicator when there is no activity
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+        /**
+         * Network traffic inactivity threshold (default is 10 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * Whether to disable showing arrows in network traffic indicators
          * @hide
          */
