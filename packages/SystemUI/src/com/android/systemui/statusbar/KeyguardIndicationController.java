0ea4c504e51a203f56551b56489e9aa39ae72136
Subhaam <derksoul98@gmail.com>
2017-06-24 03:59:39 +0000

    Revert "SystemUI: Enable show battery info by default"
    This reverts commit 02c58b85618774ca850ce849fbb17e7150376b83.

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -78,8 +78,6 @@ public class KeyguardIndicationController {
     private int mChargingWattage;
     private String mMessageToShowOnScreenOn;
 
-    boolean mShowBatteryInfo;
-
     public KeyguardIndicationController(Context context, KeyguardIndicationTextView textView,
                                         LockIcon lockIcon) {
         mContext = context;
@@ -161,9 +159,6 @@ public class KeyguardIndicationController {
 
     private void updateIndication() {
         if (mVisible) {
-           mShowBatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
-
             // Walk down a precedence-ordered list of what should indication
             // should be shown based on user or device state
             if (!mUserManager.isUserUnlocked(ActivityManager.getCurrentUser())) {
@@ -174,7 +169,7 @@ public class KeyguardIndicationController {
                 mTextView.switchIndication(mTransientIndication);
                 mTextView.setTextColor(mTransientTextColor);
 
-            } else if (mPowerPluggedIn && mShowBatteryInfo) {
+            } else if (mPowerPluggedIn) {
                 String indication = computePowerIndication();
                 if (DEBUG_CHARGING_SPEED) {
                     indication += ",  " + (mChargingWattage / 1000) + " mW";
