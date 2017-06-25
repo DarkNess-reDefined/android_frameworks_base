5dd6460dd5d83c28d52df2a50aa2d6633a59c35d
AKabhishek <abhishekkaushik.star@gmail.com>
2017-06-25 08:46:17 +0000

    Revert "GlobalActions: Confirm power off"
    This reverts commit 27f8f8eb901bf25dd636d062b017d3fd5ef425a9.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -217,7 +217,7 @@ public final class ShutdownThread extends Thread {
                                     ? com.android.internal.R.string.reboot_title
                                     : com.android.internal.R.string.power_off);
 
-            if (!advancedReboot || !mReboot || mRebootSafeMode) {
+            if (!advancedReboot || mRebootSafeMode) {
                 confirmDialogBuilder.setMessage(resourceId);
             } else {
                 confirmDialogBuilder
